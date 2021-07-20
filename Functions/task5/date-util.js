10cf9569ad823fbd9f74257117ad463cee503060
Siarhei Kandratsiuk <siarhei.kandratsiuk@epam.com>
2021-07-20 18:07:09 +0300

    refactor date-util.js

--- Functions/task5/date-util.js
+++ Functions/task5/date-util.js
@@ -1,8 +1,25 @@
 module.exports = {
     changeToMidnight: function (date, up) {
-        const newDate = new Date(date.getTime());
-        newDate.setDate(newDate.getDate() + (up ? 1 : -1));
-        newDate.setHours(0, 0, 0, 0);
+        const newDate = this.getNewDate(date);
+        this.setCorrectDate(newDate, up);
+        this.setCorrectHours(newDate);
         return newDate;
     },
+
+    getNewDate: function (date) {
+        return new Date(date.getTime());
+    },
+
+    correctDay(isUp) {
+        return isUp ? 1 : -1;
+    },
+
+    setCorrectDate(date, isUp) {
+        date.setDate(date.getDate() + this.correctDay(isUp));
+    },
+
+    setCorrectHours(date) {
+        date.setHours(0, 0, 0, 0);
+    }
+
 };
